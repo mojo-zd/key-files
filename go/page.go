b2968ac93fa06ca2e6e4f51802a3ec4009f5101c
mojo <excessivespeed@126.com>
2017-09-12 09:55:03 +0800

    page tool

---
+++ go/page.go
@@ -0,0 +1,31 @@
+package xxxx
+
+type Pagination struct {
+	Records  interface{} `json:"records"`
+	Total    int64       `json:"total"`
+	Pages    int64       `json:"pages"`
+	PageSize int64       `json:"pageSize"`
+	Current  int64       `json:"current"`
+}
+
+func (pagination *Pagination) Offset() int64 {
+	if pagination.Current == 0 {
+		return 0
+	}
+
+	return (pagination.Current - 1) * pagination.PageSize
+}
+
+func (pagination *Pagination) SetTotal(total int64) {
+	if total == 0 {
+		return
+	}
+
+	pagination.Total = total
+	if pagination.Total%pagination.PageSize > 0 {
+		pagination.Pages = pagination.Total/pagination.PageSize + 1
+	} else {
+		pagination.Pages = pagination.Total / pagination.PageSize
+	}
+}
+
